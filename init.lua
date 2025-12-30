f28f6078e2659219787cd781efb54fb9df420bd7
masaki39 <masa21gifus@gmail.com>
2025-12-30 18:47:34 +0900

    delete no status plugin

--- init.lua
+++ init.lua
@@ -1,13 +1,12 @@
 require("duckdb"):setup()
 require("full-border"):setup()
-require("no-status"):setup()
 require("bunny"):setup({
     hops = {
         { key = "h", path = "~", desc = "Home directory" },
         { key = "c", path = "~/.config", desc = "Config files" },
         { key = "v", path = "~/dev", desc = "dev" },
         { key = "d", path = "~/Downloads", desc = "Downloads" },
-        { key = "o", path = "~/Documents/ObsidianVault", desc = "Obsidian" },
+        { key = "o", path = "~/Documents/masaki39-core", desc = "Obsidian" },
         { key = "p", path = "~/pCloud Drive", desc = "pCloud Drive" }, 
         { key = "w", path = "~/pCloud Drive/Work", desc = "Work" }, 
         { key = "x", path = "~/pCloud Drive/Work/20230400-大学院", desc = "大学院" }, 
