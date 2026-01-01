dcec547dc7c943706eb3f7ba0edd082c26780edc
masaki39 <masa21gifus@gmail.com>
2026-01-01 19:58:14 +0900

    simplify

--- init.lua
+++ init.lua
@@ -7,6 +7,11 @@ require("bunny"):setup({
         { key = "o", path = "~/Documents/masaki39-core", desc = "Obsidian" },
         { key = "p", path = "~/pCloud Drive", desc = "pCloud Drive" }, 
         { key = "w", path = "~/pCloud Drive/Work", desc = "Work" }, 
-        { key = "x", path = "~/pCloud Drive/Work/20230400-大学院", desc = "大学院" }, 
+        { key = "a", path = "~/dev/analysis", desc = "analysis" }, 
     },
+    desc_strategy = "path", -- If desc isn't present, use "path" or "filename", default is "path"
+    ephemeral = true, -- Enable ephemeral hops, default is true
+    tabs = true, -- Enable tab hops, default is true
+    notify = false, -- Notify after hopping, default is false
+    fuzzy_cmd = "fzf", -- Fuzzy searching command, default is "fzf"
 })
