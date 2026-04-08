a0081ca03e636c1186f7efa30a31f602ac609aeb
masaki39 <masa21gifus@gmail.com>
2026-04-08 18:46:22 +0900

    fix: Set focus on Google Chrome in dual layout configuration

--- hammerspoon/layouts/02_dual.lua
+++ hammerspoon/layouts/02_dual.lua
@@ -3,7 +3,7 @@ return {
 	description = "Obsidian | Chrome / Ghostty",
 	windows = {
 		{ app = "Ghostty", screen = 0, x = 0, y = 0, w = 1, h = 1 },
-		{ app = "Google Chrome", screen = 1, x = 0.5, y = 0, w = 0.5, h = 1 },
-		{ app = "Obsidian", screen = 1, x = 0, y = 0, w = 0.5, h = 1, focus = true },
+		{ app = "Google Chrome", screen = 1, x = 0.5, y = 0, w = 0.5, h = 1, focus = true },
+		{ app = "Obsidian", screen = 1, x = 0, y = 0, w = 0.5, h = 1 },
 	},
 }
