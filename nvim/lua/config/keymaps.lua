dff59270a75de19c5d345d3f10036d5844d1875c
masaki39 <masa21gifus@gmail.com>
2026-04-28 20:20:28 +0900

    feat: Remove key mappings for moving lines in normal and insert modes

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -7,14 +7,6 @@ local map = vim.keymap.set
 -- 右クリックを無効化（Inspectポップアップを防ぐ）
 map({ "n", "v", "i" }, "<RightMouse>", "<Nop>", {})
 
--- Move Lines
-map("n", "<C-S-Down>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
-map("n", "<C-S-Up>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
-map("i", "<C-S-Down>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
-map("i", "<C-S-Up>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
-map("v", "<C-S-Down>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
-map("v", "<C-S-Up>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
-
 -- Insert mode: visual line movement (wrap-aware)
 map("i", "<Up>", "<C-o>gk", { desc = "Move Up (visual)" })
 map("i", "<Down>", "<C-o>gj", { desc = "Move Down (visual)" })
