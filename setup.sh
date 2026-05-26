103582d8dbcebe0bfe9bfa096decc64318520e29
masaki39 <masa21gifus@gmail.com>
2026-05-26 21:16:05 +0900

    fix: Remove unused Chrome menu shortcuts from setup script

--- setup.sh
+++ setup.sh
@@ -56,9 +56,6 @@ launchctl load "$PLIST"
 # Menu bar shortcuts
 defaults write com.google.Chrome NSUserKeyEquivalents '{
     "他のタブをすべて閉じる" = "@~w";
-    "前のタブを選択" = "@~h";
-    "次のタブを選択" = "@~l";
-    "EJDB" = "@$k";
 }'
 
 echo "✓ Setup complete!"
