9cc5b9f115001382b6e40fa5329504ff43b7b295
masaki39 <masa21gifus@gmail.com>
2026-06-04 22:17:04 +0900

    feat: Add Emacs keybindings and tab navigation shortcuts in config

--- karabiner/config.ts
+++ karabiner/config.ts
@@ -22,6 +22,19 @@ writeToProfile('Default profile', [
       .parameters({ 'basic.to_if_alone_timeout_milliseconds': 200 }),
   ]),
 
+  rule('Emacs Keybindings').manipulators([
+    map('h', 'control').to('delete_or_backspace'),
+    map('p', 'control').to('up_arrow'),
+    map('n', 'control').to('down_arrow'),
+    map('f', 'control').to('right_arrow'),
+    map('b', 'control').to('left_arrow'),
+  ]),
+
+  rule('Tab Navigation').manipulators([
+    map('h', ['command', 'option']).to('left_arrow', ['command', 'option']),
+    map('l', ['command', 'option']).to('right_arrow', ['command', 'option']),
+  ]),
+
   rule('Semicolon Hyper').manipulators([
     map('semicolon', undefined, 'any')
       .to(toHyper())
--- setup.sh
+++ setup.sh
@@ -56,8 +56,6 @@ launchctl load "$PLIST"
 # Menu bar shortcuts
 defaults write com.google.Chrome NSUserKeyEquivalents '{
     "他のタブをすべて閉じる" = "@~w";
-    "前のタブを選択" = "@~h";
-    "次のタブを選択" = "@~l";
 }'
 
 echo "✓ Setup complete!"
