42bc8763069e3000b039b4037e4aaf6e4bd566d4
masaki39 <masa21gifus@gmail.com>
2026-06-22 10:01:47 +0900

    feat: Update screen identifiers to use numerical values in layout 3

--- hammerspoon/layouts/03.lua
+++ hammerspoon/layouts/03.lua
@@ -1,9 +1,9 @@
 return {
-    keybind = "ctrl+alt+s",
-    description = "Layout 3",
-    windows = {
-        { app = "com.google.Chrome", screen = "DELL P2723DE", x = 0.7500, y = 0.0000, w = 0.2500, h = 1.0000 },
-        { app = "md.obsidian", screen = "DELL P2723DE", x = 0.0000, y = 0.0000, w = 0.2500, h = 1.0000 },
-        { app = "com.mitchellh.ghostty", screen = "DELL P2723DE", x = 0.2500, y = 0.0000, w = 0.5000, h = 1.0000, focus = true },
-    },
+	keybind = "ctrl+alt+s",
+	description = "Layout 3",
+	windows = {
+		{ app = "com.google.Chrome", screen = 1, x = 0.7500, y = 0.0000, w = 0.2500, h = 1.0000 },
+		{ app = "md.obsidian", screen = 1, x = 0.0000, y = 0.0000, w = 0.2500, h = 1.0000 },
+		{ app = "com.mitchellh.ghostty", screen = 1, x = 0.2500, y = 0.0000, w = 0.5000, h = 1.0000, focus = true },
+	},
 }
