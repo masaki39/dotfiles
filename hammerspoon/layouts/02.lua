0c2368730fb8ec4f232d08744393f8160ccc7ece
masaki39 <masa21gifus@gmail.com>
2026-07-09 12:10:54 +0900

    feat: Update window layout to use specific display names and sizes

--- hammerspoon/layouts/02.lua
+++ hammerspoon/layouts/02.lua
@@ -1,9 +1,9 @@
 return {
-	keybind = "ctrl+alt+w",
-	description = "Layout 2",
-	windows = {
-		{ app = "com.mitchellh.ghostty", screen = 0, x = 0, y = 0, w = 1, h = 1 },
-		{ app = "com.google.Chrome", screen = 1, x = 0.667, y = 0, w = 0.333, h = 1, focus = true },
-		{ app = "md.obsidian", screen = 1, x = 0, y = 0, w = 0.667, h = 1 },
-	},
+    keybind = "ctrl+alt+w",
+    description = "Layout 2",
+    windows = {
+        { app = "md.obsidian", screen = "DELL P2723DE", x = 0.0000, y = 0.0000, w = 0.5000, h = 1.0000 },
+        { app = "com.mitchellh.ghostty", screen = "Built-in Retina Display", x = 0.0000, y = 0.0000, w = 1.0000, h = 1.0000 },
+        { app = "com.google.Chrome", screen = "DELL P2723DE", x = 0.5000, y = 0.0000, w = 0.5000, h = 1.0000, focus = true },
+    },
 }
